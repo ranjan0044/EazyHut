4680aecb8cbf31d02f099b10bfbdfb0a0decbbb7
Ranjan Sarkar <ranjansarkar0044@gmail.com>
2025-07-20 13:56:58 +0530

    2nd commit

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+// includes/config.php
+// Define the base URL for the project
+
+define('BASE_URL', '/eazyhut/'); // Change if your folder name is different
+// Usage: echo BASE_URL . 'pages/login.php'; 
